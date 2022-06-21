3dddfea94aa9a1b96996aae8c52a24ad86527d85
Camarade Daniel <daniel.khamzen@capgemini.com>
2022-06-21 09:08:48 +0200

    git init tsx + tailwind

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Region from "./components/Region";
+import Footer from "./components/share/Footer";
+import Header from "./components/share/Header";
+function App() {
+  return (
+    <div className="">
+      <Header />
+      <Region />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
