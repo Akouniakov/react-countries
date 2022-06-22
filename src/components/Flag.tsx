918a2eff80bd2d36cfa560f2c191d82d9033ceaa
Akoula <akoula.dev@gmail.com>
2022-06-22 12:08:53 +0200

    :lipstick: "edit: flags component"
    Move to a grid display

--- src/components/Flag.tsx
+++ src/components/Flag.tsx
@@ -2,11 +2,12 @@ import React from "react";
 
 interface IProps {
   name: string;
+  capital: string;
 }
 
 const Flag: React.FC<IProps> = (props) => {
   return (
-    <div className="p-6 rounded-lg border">
+    <div className="p-6 rounded-lg border w-full">
       <h5 className="">{props.name}</h5>
     </div>
   );
--- src/components/Flags.tsx
+++ src/components/Flags.tsx
@@ -16,12 +16,10 @@ const Flags: React.FC<FlagsProps> = ({ region }) => {
   }, []);
 
   return (
-    <div>
-      <ul>
-        {countries.map((data, key) => (
-          <li key={key}>{data.name.official}</li>
-        ))}
-      </ul>
+    <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 2xl:grid-cols-12">
+      {countries.map((data, key) => (
+        <div key={key}>{data.name.official}</div>
+      ))}
     </div>
   );
 };
